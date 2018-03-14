d6cdfcee4c36596a851183f9c16ba47437986d47
Guillaume Parisel <guillaume.parisel6@etu.univ-lorraine.fr>
2018-03-14 15:34:16 +0100

    Ajout des classes td1

---
+++ td2/Platform.php
@@ -0,0 +1,7 @@
+<?php
+namespace td1\models;
+class Platform extends \Illuminate\Database\Eloquent\Model{
+    protected $table = 'platform';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+}
