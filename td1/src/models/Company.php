6d1073052a4e9290d70ea75cd79a25a6a287ada2
Florian <florian.cordier8@etu.univ-lorraine.fr>
2018-03-14 11:57:09 +0100

    Mod

--- td1/src/models/Company.php
+++ td1/src/models/Company.php
@@ -1,10 +1,7 @@
 <?php
 namespace td1\models;
-
 class Company extends \Illuminate\Database\Eloquent\Model{
-
     protected $table = 'company';
     protected $primaryKey = 'id';
     public $timestamps = true;
-    
 }
